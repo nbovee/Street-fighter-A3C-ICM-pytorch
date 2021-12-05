e521c82fcddb80c12ae02f01881b6d30961f57c3
nbovee <nbovine@gmail.com>
2021-12-04 22:49:14 -0500

    customizing default setup

---
+++ cheat_debug.py
@@ -0,0 +1,6 @@
+from lib.MAMEToolkit.src.MAMEToolkit.emulator import run_cheat_debugger
+roms_path = "roms/"
+game_id = "sfiii3nr1"
+run_cheat_debugger(roms_path,game_id)
+# https://github.com/Grouflon/3rd_training_lua/blob/master/3rd_training.lua
+
